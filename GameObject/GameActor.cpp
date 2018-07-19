d2d9d2962ffd3eae9dec187b50d5a059113a965d
m4jr0 <lowgravityisfun@gmail.com>
2018-07-19 19:30:33 +0200

    Empty project + Add .gitignore

--- GameObject/GameActor.cpp
+++
@@ -1,8 +0,0 @@
-/*******************************************************************************
- * Licence: http://creativecommons.org/licenses/by-nc-sa/3.0/
- * File: GameActor.cpp
- * Author: m4jr0
- * Description:
- ******************************************************************************/
-
-#include "GameActor.h"
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # gameengine
 Simple Game Engine for hobby purposes.
 
-PS: this game engine is absolutely not finished!
