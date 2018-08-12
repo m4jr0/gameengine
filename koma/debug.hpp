bb0849f156a5ecce582c7c3f25fd620e7e397c58
unknown <lowgravityisfun@gmail.com>
2018-08-12 12:19:23 +0200

    Add license boilerplate

--- koma/debug.hpp
+++ koma/debug.hpp
@@ -1,8 +1,17 @@
-#pragma once
+// Copyright 2018 m4jr0. All Rights Reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+#ifndef KOMA_DEBUG_HPP_
+#define KOMA_DEBUG_HPP_
 
 #ifdef _DEBUG
+
 #ifndef DBG_NEW
 #define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
 #define new DBG_NEW
-#endif
+#endif  // DBG_NEW
+
 #endif  // _DEBUG
+
+#endif  // KOMA_DEBUG_HPP_
